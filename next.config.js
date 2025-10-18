7ed71c37e56c97c05d674a1b1271a0caae3c9bd2
LEONEL EGIDIO DE FREITAS <leonel.efreitas@gmail.com>
2025-10-18 13:06:20 -0300

    atual

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ignora erros de lint durante o build (útil para deploy rápido)
     ignoreDuringBuilds: true,
   },
 };
